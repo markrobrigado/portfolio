98e24c09a5714c3c4e892bcc274adb0b1673d221
Mark Robrigado <mrobrigado.dev@gmail.com>
2024-11-09 13:07:27 +0800

    Update header and footer layout; transform nav items to links.

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,5 +1,7 @@
-<div class="h-1/8 grid grid-cols-2">
+<div class="fixed bottom-0 left-0 w-full h-20 p-8 flex items-center justify-center">
     <!-- Footer Content -->
-    <p class="text-3xl text-start">contact</p>
-    <p class="text-3xl text-end">scroll ▼ for more</p>
+    <nav class="flex justify-between w-full">
+        <a href="#contact" class="text-3xl text-start hover:underline">contact</a>
+        <p class="text-3xl text-end">scroll ▼ for more</p>
+    </nav>
 </div>
--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,6 +1,8 @@
-<header class="h-1/8  grid grid-cols-3">
+<header class="fixed top-0 left-0 w-full h-20 p-8 flex items-center justify-center">
     <!-- Header Content -->
-    <p class="text-3xl text-start">home</p>
-    <p class="text-3xl text-center">projects</p>
-    <p class="text-3xl text-end">about</p>
+    <nav class="flex justify-between w-full">
+        <a href="#home" class="text-3xl text-start hover:underline">home</a>
+        <a href="#projects" class="text-3xl text-center hover:underline">projects</a>
+        <a href="#about" class="text-3xl text-end hover:underline">about</a>
+    </nav>
 </header>
