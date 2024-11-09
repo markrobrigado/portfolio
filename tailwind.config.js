9e4b38007b784f39aa585d3fcf6aff9e99683501
Mark Robrigado <mrobrigado.dev@gmail.com>
2024-11-09 12:18:38 +0800

    Add default font family and custom typing animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,25 @@
 export default {
   content: ["./src/**/*.{html,js,svelte,ts}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ['"Helvetica"', "sans-serif"],
+      mono: ['"Consolas"', "monospace"],
+    },
+    extend: {
+      animation: {
+        typing:
+          "typing 1s steps(6) forwards, cursor 0.5s step-end infinite alternate",
+      },
+      keyframes: {
+        typing: {
+          from: { width: "0" },
+          to: { width: "100%" },
+        },
+        cursor: {
+          "50%": { borderColor: "transparent" },
+        },
+      },
+    },
   },
   plugins: [],
 };
