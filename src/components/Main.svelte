02ce377ac37738b21e23960c519c671c74c4ec38
Mark Robrigado <mrobrigado.dev@gmail.com>
2024-11-11 12:32:37 +0800

    Add About, Contact, Projects section to Main component.

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -1,7 +1,24 @@
 <script>
-    import Home from '../components/sections/Home.svelte'
+  import Home from "./sections/Home.svelte";
+  import About from "./sections/About.svelte";
+  import Contact from "./sections/Contact.svelte";
+  import Projects from "./sections/Projects.svelte";
 </script>
 
-<section class="flex flex-row">
+<div class="flex flex-col">
+  <section id="home">
     <Home />
-</section>
+  </section>
+
+  <section id="about">
+    <About />
+  </section>
+
+  <section id="projects">
+    <Projects />
+  </section>
+
+  <section id="contact">
+    <Contact />
+  </section>
+</div>
