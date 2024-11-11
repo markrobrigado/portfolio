91b7db242aee2f5bbdcc1e7d9871e08e03927190
Mark Robrigado <mrobrigado.dev@gmail.com>
2024-11-11 12:24:17 +0800

    Add About section component.

---
+++ src/components/sections/About.svelte
@@ -0,0 +1,14 @@
+<div
+  class="flex flex-col h-auto min-h-screen items-center justify-center p-8 bg-blue-700"
+>
+  <span
+    class="font-black text-white text-3xl md:text-5xl text-center leading-tight"
+    >① hello👋 i am Mark Robrigado, a passionate software developer; ② i
+    previously worked as a cybersecurity analyst &#91;PwC, '23-'24&#93; and as a
+    backend software engineer intern &#91;Suiterus, '22&#93;; ③ i graduated in
+    2022 from Polytechnic University of the Philippines with a degree in
+    computer science; ④ take a look at some of my
+    <a href="#projects" class="underline">work</a> or say
+    <a href="#contact" class="underline">hello</a></span
+  >
+</div>
